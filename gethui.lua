12c81e853aabc4777b5b7efab083ce99b347350d
MrY7zz <handletest03@gmail.com>
2026-08-10 23:39:22 -0300

    sUNC hates non folder, gethui is stupid anyways

--- gethui.lua
+++ gethui.lua
@@ -1,14 +1,14 @@
 -- gethui.lua
 -- © 2026 MrY7zz (ISC License)
 
-local SafeScreenGui, ChosenGuiService = Instance.new("ScreenGui"), cloneref and cloneref(game:FindFirstChildOfClass("CoreGui") or game:GetService("Players").LocalPlayer:FindFirstChildOfClass("PlayerGui")) or game:FindFirstChildOfClass("CoreGui") or game:GetService("Players").LocalPlayer:FindFirstChildOfClass("PlayerGui")
+local SafeScreenGui, ChosenGuiService = Instance.new("Folder"), cloneref and cloneref(game:FindFirstChildOfClass("CoreGui") or game:GetService("Players").LocalPlayer:FindFirstChildOfClass("PlayerGui")) or game:FindFirstChildOfClass("CoreGui") or game:GetService("Players").LocalPlayer:FindFirstChildOfClass("PlayerGui")
 SafeScreenGui.Name = game:GetService("HttpService"):GenerateGUID(false)
 pcall(function()
 	SafeScreenGui.RobloxLocked = true
 end)
 SafeScreenGui.Parent = ChosenGuiService
 
-function gethui()
+function gethui(): BasePlayerGui | Folder
 	return SafeScreenGui
 end
 
