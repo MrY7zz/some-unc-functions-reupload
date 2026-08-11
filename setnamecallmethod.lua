57592f975894ceb2fa2b16e5fc33e1ac650e810d
MrY7zz <handletest03@gmail.com>
2026-08-10 23:23:55 -0300

    Reupload setnamecallmethod.lua

---
+++ setnamecallmethod.lua
@@ -0,0 +1,43 @@
+-- setnamecallmethod.lua
+-- © 2026 MrY7zz (ISC License)
+
+local _Instance = Instance.new("BuoyancySensor")
+_Instance.Destroy(_Instance)
+
+function setnamecallmethod(method)
+	if type(method) ~= "string" then
+		return error("invalid argument #1 to 'setnamecallmethod' (string expected, got " .. typeof(method) .. ")")
+	end
+	pcall(loadstring("(...):" .. method .. "()"), _Instance)
+end
+
+--[=[
+Usage:
+
+local _, namecall = xpcall(function()
+	return game:__namecall()
+end, function()
+	return debug.info(2, "f")
+end)
+
+local patched__namecall = function(instance, method, ...)
+	setnamecallmethod(method)
+
+	return namecall(instance, ...)
+end
+
+print(patched__namecall(game, "FindFirstChild", "Workspace")) --> Output: Workspace
+
+--> Or you can do this: <--
+
+local _, namecall = xpcall(function()
+	return game:__namecall()
+end, function()
+	return debug.info(2, "f")
+end)
+
+setnamecallmethod("FindFirstChild")
+
+print(namecall(game, "Workspace"))
+]=]
+
